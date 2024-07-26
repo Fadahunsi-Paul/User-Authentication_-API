90b75a448a4896ff36beeffdd5b4a7d6faee0fb4
unknown <scandiclamar@gmail.com>
2024-07-26 03:01:25 +0000

    new

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'',RegistrationViewSet,basename='register')
 router.register(r'',LoginViewset,basename='login')
-router.register(r'',VerifyEmailViewSet,basename='verify')
+router.register(r'verify',VerifyEmailViewSet,basename='verify')
 
 urlpatterns = [
     path('',include(router.urls)),
